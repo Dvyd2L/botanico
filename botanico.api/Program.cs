6aeb15ee89c03e1412d36d364460293778d87b12
ARELANCE\david.llopis <david.llopis@arelance.com>
2024-06-05 11:57:13 +0200

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   .gitignore
    	modified:   botanico.api/Program.cs

--- botanico.api/Program.cs
+++ botanico.api/Program.cs
@@ -11,66 +11,61 @@ Esp32.Esp32Client esp32client = new(channel);
 
 Dictionary<String, Delegate> getHandlers = new()
 {
+  {
+    "/",
+    async () =>
     {
-        "/",
-        async () =>
-        {
-            // Llama al servicio gRPC
-            HelloReply reply = await greeterClient.SayHelloAsync(new HelloRequest { Name = "World" });
-
-            // Devuelve la respuesta del servicio gRPC como respuesta HTTP
-            return reply.Message;
-        }
-    },
-    {
-        "/{name}",
-        async ([FromRoute] String name) =>
-        {
-            // Llama al servicio gRPC
-            HelloReply reply = await greeterClient.SayHelloAsync(new HelloRequest { Name = name });
-
-            // Devuelve la respuesta del servicio gRPC como respuesta HTTP
-            return reply.Message;
-        }
-    },
-    {
-        "/esp32/toggle-led",
-        async () =>
-        {
-            // Llama al servicio gRPC
-            ChangeStatusLedReply reply = await esp32client.ToggleLedAsync(new ChangeStatusLedRequest { Status = false });
-
-            // Devuelve la respuesta del servicio gRPC como respuesta HTTP
-            return reply.Status;
-        }
-    },
-    {
-        "/esp32/turn-off-led",
-        async () =>
-        {
-            // Llama al servicio gRPC
-            ChangeStatusLedReply reply = await esp32client.TurnOffLedAsync(new ChangeStatusLedRequest { Status = false });
-
-            // Devuelve la respuesta del servicio gRPC como respuesta HTTP
-            return reply.Status;
-        }
-    },
-    {
-        "/esp32/turn-on-led",
-        async () =>
-        {
-            // Llama al servicio gRPC
-            ChangeStatusLedReply reply = await esp32client.TurnOnLedAsync(new ChangeStatusLedRequest { Status = true });
-
-            // Devuelve la respuesta del servicio gRPC como respuesta HTTP
-            return reply.Status;
-        }
+      // Llama al servicio gRPC
+      HelloReply reply = await greeterClient.SayHelloAsync(new HelloRequest { Name = "World" });
+      // Devuelve la respuesta del servicio gRPC como respuesta HTTP
+      return reply.Message;
     }
+  },
+  {
+      "/{name}",
+      async ([FromRoute] String name) =>
+      {
+          // Llama al servicio gRPC
+          HelloReply reply = await greeterClient.SayHelloAsync(new HelloRequest { Name = name });
+          // Devuelve la respuesta del servicio gRPC como respuesta HTTP
+          return reply.Message;
+      }
+  },
+  {
+      "/esp32/toggle-led",
+      async () =>
+      {
+          // Llama al servicio gRPC
+          ChangeStatusLedReply reply = await esp32client.ToggleLedAsync(new ChangeStatusLedRequest { Status = false });
+          // Devuelve la respuesta del servicio gRPC como respuesta HTTP
+          return reply.Status;
+      }
+  },
+  {
+      "/esp32/turn-off-led",
+      async () =>
+      {
+          // Llama al servicio gRPC
+          ChangeStatusLedReply reply = await esp32client.TurnOffLedAsync(new ChangeStatusLedRequest { Status = false });
+          // Devuelve la respuesta del servicio gRPC como respuesta HTTP
+          return reply.Status;
+      }
+  },
+  {
+      "/esp32/turn-on-led",
+      async () =>
+      {
+          // Llama al servicio gRPC
+          ChangeStatusLedReply reply = await esp32client.TurnOnLedAsync(new ChangeStatusLedRequest { Status = true });
+          // Devuelve la respuesta del servicio gRPC como respuesta HTTP
+          return reply.Status;
+      }
+  }
 };
 
 foreach (KeyValuePair<String, Delegate> entry in getHandlers)
 {
-    _ = app.MapGet(entry.Key, entry.Value);
+  _ = app.MapGet(entry.Key, entry.Value);
 }
 
 app.Run();
